e1d922776280307277a8a2f8c900efd84789c555
DedHorse <69487257+DedHorse@users.noreply.github.com>
2023-10-05 22:36:18 +0000

    Rework for main
    changed to http 0.9

--- main.c
+++ main.c
@@ -1,144 +1,176 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
+#include <signal.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int getRequest( int conn, char* req_buf )
+int CTRL_C_PRESSED = 0;
+
+void sendResource( char* webroot, char* resource, int conn )
 {
-	char filename_buf[99];
-	memset( filename_buf, 0, sizeof( filename_buf ) );
-	int i = sizeof( "GET" ) + 1;
-	int j = 0;
-	while( req_buf[i] != ' ' )
-	{
-		filename_buf[j] = req_buf[i];
-		++i;
-		++j;
-	}
+	char full_path[ strlen(webroot) + strlen(resource) ];
+	strcpy( full_path, webroot );
+	strcat( full_path, resource );
 
-	char http_ver_buf[99];
-	memset( http_ver_buf, 0, sizeof(http_ver_buf) );
-	j = 0;
-	while( req_buf[i] != '\r' && req_buf[i+1] != '\n' )
-	{
-		http_ver_buf[j] = req_buf[i];
-		++i;
-		++j;
-	}
+	//printf("%s\n", full_path);
 
-	printf( "[*] File requested: %s\n", filename_buf );
-	printf( "[*] HTTP version: %s\n", http_ver_buf );
+	int fd = open( full_path, O_RDONLY );
+
+	if( fd <= 0 )
+	{
+		char* file_not_found = "<html>\nNo such file\n</html>";
+		send( conn, file_not_found, strlen( file_not_found ), 0 );
+		close( conn );
+		printf( "(Not found)\n" );
+		return;
+	}
 	
-	char file_buf[99];
+	FILE* fp = fdopen( fd, "r" );
+	fseek( fp, 0L, SEEK_END );
+	int file_size = ftell( fp );
+	rewind( fp );
 
-	int fd = open( filename_buf, O_RDONLY );
-	if( fd < 0 )
-	{
-		//return 404
-		printf("404\n");
-		return 0;
-	}
+	char* file_content = malloc( file_size ); 
 
-	if( read( fd, file_buf, sizeof( file_buf ) ) < 0 )
+	if( fread( file_content, sizeof( *file_content ), file_size, fp ) != file_size )
 	{
 		perror( "[-] Failed to read file" );
-		exit( EXIT_FAILURE );
+		fclose( fp );
+		free( file_content );
+		return;
 	}
 
-	close( fd );
+	fclose( fp );
 
-	printf( "[*] Requested content: %s\n", file_buf );
+	send( conn, file_content, file_size, 0 );
 
-	send( conn, file_buf, sizeof(file_buf), 0 );	
+	printf( "(Found)\n" );
 
-	return 0;
+	free( file_content );
 }
 
-int handleClient( int conn )
+void parseRequestedResource( char* buf, char* resource )
 {
-	char buf[99];
-	memset( buf, 0, sizeof( buf ) );
-
-	recv( conn, buf, sizeof( buf ), 0 );
-
-	printf( buf );
-
-	char method_buf[6];
-	memset( method_buf, 0, sizeof( method_buf ) );
 	int i = 0;
 	while( buf[i] != ' ' )
-	{
-		method_buf[i] = buf[i];
 		++i;
+
+	++i;		// hack to skip the space
+	int j = 0;
+
+	while( buf[i] != '\n' )
+	{
+		if( buf[i] != '\r' )
+			resource[j] = buf[i];
+		++i;
+		++j;
 	}
-
-	printf( "[*] Request method: %s\n", method_buf );	
-
-	if( strcmp( method_buf, "GET" ) == 0 )
-		getRequest( conn, buf );
-
-	return 0;
 }
 
-int main( int arcg, char** argv )
+void handleConnection( int conn, char* webroot )
 {
-	char* addr = "127.0.0.1";
-	uint16_t port = 80;
+	char buf[1024];
+	memset( buf, 0, sizeof( buf ) );
+	recv( conn, buf, sizeof( buf ), 0 );
 
+	char resource[1024];
+	memset( resource, 0, sizeof( resource ) );
+	parseRequestedResource( buf, resource );
+
+	printf( "[*] Client requested %s ", resource ); // status gets printed by send resource
+
+	sendResource( webroot, resource, conn );
+	close( conn );
+}
+
+int createSocket( char* host, int port )
+{
+	struct sockaddr_in name;
 	int sock = socket( PF_INET, SOCK_STREAM, 0 );
 	if( sock < 0 )
+		return -1;
+	
+	name.sin_family = AF_INET;
+	name.sin_port = htons( port );
+	name.sin_addr.s_addr = inet_addr( host );
+
+	const int enable = 1;
+	if( setsockopt( sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof( enable ) ) < 0 )
 	{
-		perror( "[-] Couldn't create socket!\n" );
+		perror( "[-] Failed to set sock options" );
 		exit( EXIT_FAILURE );
 	}
 
-	const int enable_opt = 1;
+	if( bind( sock, (struct sockaddr*) &name, sizeof( name ) ) < 0 )
+		return -2;
 
-	if( setsockopt( sock, SOL_SOCKET, SO_REUSEADDR, &enable_opt, sizeof(int) ) < 0 )
+	return sock;
+}
+
+void exit_prog( int sig_num )
+{
+	CTRL_C_PRESSED = 1;
+}
+
+int main(void)
+{
+	printf( "[*] Starting.\n" );
+
+	signal(SIGINT, exit_prog);
+
+	char* webroot = ".";
+	// setup socket
+	char* host = "127.0.0.1";
+	int port = 80;	
+	int sock = createSocket( host, port );
+
+	if( sock == -1 )
 	{
-		perror( "[-] Couldn't set socket options.\n" );
+		perror( "[-] Failed to create socket.\n" );
+		exit( EXIT_FAILURE );
+	}
+	if( sock == -2 )
+	{
+		perror( "[-] Failed to bind socket.\n" );
 		exit( EXIT_FAILURE );
 	}
 
-	struct sockaddr_in name;
-	name.sin_family 	= 	AF_INET;
-	name.sin_port 		= 	htons( port );
-	name.sin_addr.s_addr 	=	inet_addr(addr);
-
-	int name_size = sizeof( name );
-
-	if( bind( sock, (struct sockaddr *) &name, name_size ) < 0 )
-	{
-		perror( "[-] Couldn't bind socket\n" );
-		exit( EXIT_FAILURE );
-	}
+	printf( "[+] Socket created successfuly.\n" );
 
 	struct sockaddr_in client;
-	int client_size = sizeof(client);
+	int client_size = sizeof( client );
 
-	while( 1 )
+	printf( "[+] Listnening for connections.\n" );
+	
+	// TODO figure out how to handle this all with nonblocking I/O and threads.
+	
+	while( !CTRL_C_PRESSED )
 	{
+	// listen for connections
 		listen( sock, 10 );
-		int conn = accept( (int) sock, (struct sockadrr *) &client, &client_size );
-		if(conn < 0)
+	// handle connections
+		int conn = accept( sock, (struct sockaddr*) &client, &client_size );
+		if( conn < 0 )
 		{
-			printf( "[-] Failed to accept connection.\n" );
-			perror( EXIT_FAILURE );
+			perror( "[-] Failed to accept connection.\n" );
+			continue;
 		}
+	
+		char client_ip[INET_ADDRSTRLEN];
+		inet_ntop( AF_INET, &client.sin_addr, client_ip, sizeof( client_ip ) );
+		printf( "[*] Connection from %s on port %d.\n", client_ip, ntohs( client.sin_port ) );
 
-		char client_ip_buf[INET_ADDRSTRLEN];
-		inet_ntop( AF_INET, &client.sin_addr, client_ip_buf, sizeof(client_ip_buf) );
-		printf( "[*] Connection from %s:%d\n", client_ip_buf, ntohs( client.sin_port ) );
-		
-		handleClient( conn );
+		handleConnection( conn, webroot );
 
-		break;
+	// back to loop, if ctrl+c pressed break
 	}
-
+	// clean up
+	
 	close( socket );
 	printf( "[*] Exiting.\n" );
+
 	return 0;
 }
